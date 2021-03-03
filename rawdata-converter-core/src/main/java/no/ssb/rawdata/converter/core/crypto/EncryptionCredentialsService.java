0136d095a941f2014e01de5cca42aeec51dd4203
Kenneth Leine Schulstad <ks@udp.no>
2021-03-03 23:02:13 +0100

    Add MockEncryptionCredentialsService
    Used in tests where gcp credentials is unable to be specified

--- rawdata-converter-core/src/main/java/no/ssb/rawdata/converter/core/crypto/EncryptionCredentialsService.java
+++ rawdata-converter-core/src/main/java/no/ssb/rawdata/converter/core/crypto/EncryptionCredentialsService.java
@@ -3,10 +3,18 @@ package no.ssb.rawdata.converter.core.crypto;
 public interface EncryptionCredentialsService {
 
     /**
-     * Resolve rawdata encryption key
+     * Resolve latest version of rawdata encryption key
      * @param keyId id of rawdata encryption key to resolve
-     * @return the rawdata encryption key for the specified id
+     * @return the rawdata encryption key for the specified id (latest version)
      */
     char[] getKey(String keyId);
+
+    /**
+     * Resolve a specific version of a rawdata encryption key
+     * @param keyId id of rawdata encryption key to resolve
+     * @param version version of rawdata encryption key to resolve
+     * @return the rawdata encryption key for the specified id and version
+     */
+    char[] getKey(String keyId, String version);
 
 }
--- rawdata-converter-core/src/main/java/no/ssb/rawdata/converter/core/crypto/SecretManagerEncryptionCredentialsService.java
+++ rawdata-converter-core/src/main/java/no/ssb/rawdata/converter/core/crypto/SecretManagerEncryptionCredentialsService.java
@@ -1,5 +1,7 @@
 package no.ssb.rawdata.converter.core.crypto;
 
+import io.micronaut.context.annotation.Primary;
+import io.micronaut.context.annotation.Requires;
 import io.micronaut.gcp.secretmanager.client.SecretManagerClient;
 import lombok.RequiredArgsConstructor;
 import no.ssb.rawdata.converter.core.exception.RawdataConverterException;
@@ -9,22 +11,28 @@ import java.nio.charset.StandardCharsets;
 
 @RequiredArgsConstructor
 @Singleton
+@Primary
+@Requires(property = "gcp.project-id")
 public class SecretManagerEncryptionCredentialsService implements EncryptionCredentialsService {
 
     private final SecretManagerClient secretManager;
 
     @Override
     public char[] getKey(String keyId) {
-        byte[] key = fetchSecret(keyId);
+        return getKey(keyId, "latest");
+    }
+
+    @Override
+    public char[] getKey(String keyId, String version) {
+        byte[] key = fetchSecret(keyId, version);
         return new String(key, StandardCharsets.UTF_8).toCharArray();
     }
 
-    byte[] fetchSecret(String secretId) {
+    byte[] fetchSecret(String secretId, String version) {
         try {
-            return secretManager.getSecret(secretId).blockingGet().getContents();
-        }
-        catch (Exception e) {
-            throw new SecretManagerEncryptionCredentialsServiceException("Error fetching secret with id " + secretId + " from secret manager", e);
+            return secretManager.getSecret(secretId, version).blockingGet().getContents();
+        } catch (Exception e) {
+            throw new SecretManagerEncryptionCredentialsServiceException("Error fetching secret with id '" + secretId + "' and version=" + version + " from secret manager", e);
         }
     }
 
@@ -32,6 +40,6 @@ public class SecretManagerEncryptionCredentialsService implements EncryptionCred
         public SecretManagerEncryptionCredentialsServiceException(String message, Throwable cause) {
             super(message, cause);
         }
-   }
+    }
 
 }
