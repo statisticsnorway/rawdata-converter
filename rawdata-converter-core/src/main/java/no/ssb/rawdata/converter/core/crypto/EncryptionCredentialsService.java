96c6bfc973ef23d6c1f19e4acd4005856752f29d
Kenneth Leine Schulstad <ks@udp.no>
2021-03-09 14:33:43 +0100

    Use SecretService in EncryptionCredentialsService

--- rawdata-converter-core/src/main/java/no/ssb/rawdata/converter/core/crypto/EncryptionCredentialsService.java
+++ rawdata-converter-core/src/main/java/no/ssb/rawdata/converter/core/crypto/EncryptionCredentialsService.java
@@ -1,13 +1,25 @@
 package no.ssb.rawdata.converter.core.crypto;
 
-public interface EncryptionCredentialsService {
+import lombok.RequiredArgsConstructor;
+import no.ssb.rawdata.converter.service.secret.SecretService;
+
+import javax.inject.Singleton;
+import java.nio.charset.StandardCharsets;
+
+@Singleton
+@RequiredArgsConstructor
+public class EncryptionCredentialsService {
+
+    private final SecretService secretService;
 
     /**
      * Resolve latest version of rawdata encryption key
      * @param keyId id of rawdata encryption key to resolve
      * @return the rawdata encryption key for the specified id (latest version)
      */
-    char[] getKey(String keyId);
+    char[] getKey(String keyId) {
+        return new String(secretService.getSecret(keyId), StandardCharsets.UTF_8).toCharArray();
+    }
 
     /**
      * Resolve a specific version of a rawdata encryption key
@@ -15,6 +27,8 @@ public interface EncryptionCredentialsService {
      * @param version version of rawdata encryption key to resolve
      * @return the rawdata encryption key for the specified id and version
      */
-    char[] getKey(String keyId, String version);
+    char[] getKey(String keyId, String version) {
+        return new String(secretService.getSecret(keyId, version), StandardCharsets.UTF_8).toCharArray();
+    }
 
 }
