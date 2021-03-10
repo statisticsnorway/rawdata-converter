9fa217b7aad9d81b1805449f2018d47b5ef5d6d4
Kenneth Leine Schulstad <ks@udp.no>
2021-03-10 09:02:55 +0100

    Update test

--- rawdata-converter-core/src/test/java/no/ssb/rawdata/converter/service/secret/GcpSecretServiceTest.java
+++ rawdata-converter-core/src/test/java/no/ssb/rawdata/converter/service/secret/GcpSecretServiceTest.java
@@ -1,5 +1,6 @@
 package no.ssb.rawdata.converter.service.secret;
 
+import io.micronaut.context.annotation.Property;
 import io.micronaut.gcp.secretmanager.client.SecretManagerClient;
 import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @MicronautTest(environments = "test-gcp")
+@Property(name = "services.secrets.impl", value = "GCP")
 class GcpSecretServiceTest {
 
     @Inject SecretServiceConfig config;
