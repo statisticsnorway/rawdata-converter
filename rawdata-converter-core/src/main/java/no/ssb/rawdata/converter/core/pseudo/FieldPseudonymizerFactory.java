b717361b186cbad055cdad4acab73032e15d5563
Kenneth Leine Schulstad <ks@udp.no>
2021-03-10 20:29:22 +0100

    Retrieve pseudo secrets via SecretService

--- rawdata-converter-core/src/main/java/no/ssb/rawdata/converter/core/pseudo/FieldPseudonymizerFactory.java
+++ rawdata-converter-core/src/main/java/no/ssb/rawdata/converter/core/pseudo/FieldPseudonymizerFactory.java
@@ -1,16 +1,17 @@
 package no.ssb.rawdata.converter.core.pseudo;
 
-import com.google.common.annotations.VisibleForTesting;
 import io.micronaut.context.annotation.Property;
 import lombok.extern.slf4j.Slf4j;
 import no.ssb.dlp.pseudo.core.FieldPseudonymizer;
 import no.ssb.dlp.pseudo.core.PseudoSecret;
+import no.ssb.rawdata.converter.core.exception.RawdataConverterException;
 import no.ssb.rawdata.converter.core.job.ConverterJobConfig;
 import no.ssb.rawdata.converter.service.secret.SecretService;
 
 import javax.inject.Singleton;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -23,7 +24,7 @@ public class FieldPseudonymizerFactory {
 
     private final static String DEFAULT_PSEUDO_SECRET_TYPE = "AES256";
     private final SecretService secretService;
-    private final Set<PseudoSecret> pseudoSecrets;
+    private final Map<String, PseudoSecret> configuredPseudoSecrets;
 
     /**
      * Initialize the FieldPseudonymizerFactory
@@ -32,20 +33,44 @@ public class FieldPseudonymizerFactory {
      */
     public FieldPseudonymizerFactory(SecretService secretService, @Property(name="pseudo.secrets") Map<String, PseudoSecret> configuredPseudoSecrets) {
         this.secretService = secretService;
-        this.pseudoSecrets = configuredPseudoSecrets.entrySet().stream()
+        this.configuredPseudoSecrets = Optional.ofNullable(configuredPseudoSecrets).orElse(Map.of());
+    }
+
+    public FieldPseudonymizer newFieldPseudonymizer(ConverterJobConfig jobConfig) {
+        return new FieldPseudonymizer.Builder()
+          .rules(jobConfig.getPseudoRules())
+          .secrets(resolvePseudoSecrets((configuredPseudoSecrets)))
+          .build();
+    }
+
+    /**
+     * <p>Clean up configured pseudo secrets and resolve pseudo secret contents.</p>
+     *
+     * <p>If content is specified, then this is used. If not, attempt to resolve pseudo secret from SecretService using
+     * the PseudoSecret::id and PseudoSecret::version properties.</p>
+     *
+     * @param configuredPseudoSecrets a Map named pseudo secrets
+     * @return a List of cleaned up, resolved pseudo secrets
+     */
+    List<PseudoSecret> resolvePseudoSecrets(Map<String, PseudoSecret> configuredPseudoSecrets) {
+        if (configuredPseudoSecrets == null) {
+            return List.of();
+        }
+
+        return configuredPseudoSecrets.entrySet().stream()
           .map(e -> {
               PseudoSecret secret = e.getValue();
 
-              // Resolve secret content if and only if ID is specified AND content is not specified
+              secret.setName(e.getKey());
+
+              // Resolve secret content if and only if 'id' is specified AND 'content' is not specified
               if (secret.getId() != null && secret.getContent() == null) {
-                  secret.setContent(secretService.getCacheableSecret(secret.getId()));
+                  secret.setBase64EncodedContent(secretService.getCacheableSecret(secret.getId(), secret.getVersion()));
               }
 
               if (secret.getContent() == null) {
-                  log.warn("Invalid pseudo secret '{}': Unable to resolve content", e.getKey());
+                  throw new InvalidPseudoSecretException("Invalid pseudo secret '" + e.getKey() + "': Unable to resolve content");
               }
-
-              secret.setId(e.getKey()); // Pseudo secrets use the mapping name as id
 
               if (secret.getType() == null) {
                   secret.setType(DEFAULT_PSEUDO_SECRET_TYPE);
@@ -53,19 +78,13 @@ public class FieldPseudonymizerFactory {
 
               return secret;
           })
-          .collect(Collectors.toSet());
+          .collect(Collectors.toList());
     }
 
-    public FieldPseudonymizer newFieldPseudonymizer(ConverterJobConfig jobConfig) {
-        return new FieldPseudonymizer.Builder()
-          .rules(jobConfig.getPseudoRules())
-          .secrets(pseudoSecrets)
-          .build();
-    }
-
-    @VisibleForTesting
-    Set<PseudoSecret> getPseudoSecrets() {
-        return pseudoSecrets;
+    class InvalidPseudoSecretException extends RawdataConverterException {
+        public InvalidPseudoSecretException(String message) {
+            super(message);
+        }
     }
 
 }
--- rawdata-converter-core/src/test/java/no/ssb/rawdata/converter/core/pseudo/FieldPseudonymizerFactoryTest.java
+++ rawdata-converter-core/src/test/java/no/ssb/rawdata/converter/core/pseudo/FieldPseudonymizerFactoryTest.java
@@ -1,11 +1,18 @@
 package no.ssb.rawdata.converter.core.pseudo;
 
+import io.micronaut.context.annotation.Property;
 import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import no.ssb.dlp.pseudo.core.PseudoSecret;
 import no.ssb.rawdata.converter.core.convert.RawdataConverterFactory;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @MicronautTest(environments = "test-pseudo")
 class FieldPseudonymizerFactoryTest {
@@ -17,10 +24,71 @@ class FieldPseudonymizerFactoryTest {
         return jobConfig -> null;
     }
 
+    @Property(name="pseudo.secrets")
+    Map<String, PseudoSecret> configuredPseudoSecrets;
+
     @Test
-    void testStuff() {
-        System.out.println();
-        fieldPseudonymizerFactory.getPseudoSecrets();
-        fieldPseudonymizerFactory.newFieldPseudonymizer(null);
+    void givenEmptyConfiguredPseudoSecrets_whenResolvePseudoSecrets_thenShouldProduceEmptyListOfPseudoSecrets() {
+        assertThat(fieldPseudonymizerFactory.resolvePseudoSecrets(Map.of())).isEqualTo(Collections.EMPTY_LIST);
+        assertThat(fieldPseudonymizerFactory.resolvePseudoSecrets(null)).isEqualTo(Collections.EMPTY_LIST);
     }
+
+    /**
+     * References pseudo secrets configured in src/test/resources/application-test-pseudo.yml)
+     *
+     * pseudo.secrets:
+     *   testsecret1:
+     *     content: dGhlX3NlY3JldF9wYXNzd29yZF9pc19rZW5zZW50bWU=
+     *     type: AES256
+     *   testsecret2:
+     *     id: pseudo-secret-testsecret2
+     *     content: dGhlX3NlY3JldF9wYXNzd29yZF9pc19rZW5zZW50bWU=
+     *   testsecret3:
+     *     id: pseudo-secret-testsecret3
+     *     type: AES256
+     *   pseudo-secret-testsecret4:
+     *     id: pseudo-secret-testsecret4
+     *     version: 2
+     *
+     * services:
+     *   secrets:
+     *     impl: LOCAL
+     *     overrides:
+     *       pseudo-secret-testsecret3: supersecret3lAwuVx6NuAsMWLusOSA/ldia40ZugDI=
+     *       pseudo-secret-testsecret4: supersecret4lAwuVx6NuAsMWLusOSA/ldia40ZugDI=
+     *
+     */
+    @Test
+    void givenConfiguredPseudoSecrets_whenResolvePseudoSecrets_thenShouldProduceListOfValidPseudoSecrets() {
+        List<PseudoSecret> pseudoSecrets = fieldPseudonymizerFactory.resolvePseudoSecrets(configuredPseudoSecrets);
+
+        PseudoSecret secret = pseudoSecrets.get(0);
+        assertThat(secret.getName()).isEqualTo("testsecret1");
+        assertThat(secret.getId()).isNull();
+        assertThat(secret.getVersion()).isNull();
+        assertThat(secret.getBase64EncodedContent()).isEqualTo("dGhlX3NlY3JldF9wYXNzd29yZF9pc19rZW5zZW50bWU=");
+        assertThat(secret.getType()).isEqualTo("AES256");
+
+        secret = pseudoSecrets.get(1);
+        assertThat(secret.getName()).isEqualTo("testsecret2");
+        assertThat(secret.getId()).isEqualTo("pseudo-secret-testsecret2");
+        assertThat(secret.getVersion()).isNull();
+        assertThat(secret.getBase64EncodedContent()).isEqualTo("dGhlX3NlY3JldF9wYXNzd29yZF9pc19rZW5zZW50bWU=");
+        assertThat(secret.getType()).isEqualTo("AES256");
+
+        secret = pseudoSecrets.get(2);
+        assertThat(secret.getName()).isEqualTo("testsecret3");
+        assertThat(secret.getId()).isEqualTo("pseudo-secret-testsecret3");
+        assertThat(secret.getVersion()).isNull();
+        assertThat(secret.getBase64EncodedContent()).isEqualTo("supersecret3lAwuVx6NuAsMWLusOSA/ldia40ZugDI=");
+        assertThat(secret.getType()).isEqualTo("AES256");
+
+        secret = pseudoSecrets.get(3);
+        assertThat(secret.getName()).isEqualTo("pseudo-secret-testsecret4");
+        assertThat(secret.getId()).isEqualTo("pseudo-secret-testsecret4");
+        assertThat(secret.getVersion()).isEqualTo("2");
+        assertThat(secret.getBase64EncodedContent()).isEqualTo("supersecret4lAwuVx6NuAsMWLusOSA/ldia40ZugDI=");
+        assertThat(secret.getType()).isEqualTo("AES256");
+    }
+
 }
